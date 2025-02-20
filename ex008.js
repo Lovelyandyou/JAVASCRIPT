08544cdf4eda5a8e1b6b30d3c5542bab89519f3e
Brenda Rafaele Soares Da Silva <103004471+Lovelyandyou@users.noreply.github.com>
2025-02-20 14:45:03 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 75.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você ultrapssou a velocidade permitida. MULTADO!')
+
+}
+console.log('Dirija sempre usando cinto de segurança.!')
